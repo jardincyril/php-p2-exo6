6e056d9543cd42fa6d76f8ba3f04f47bfe03ecb9
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:36:06 +0200

    condition

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo6</title>
+  </head>
+  <body>
+    <?php
+    $age = 22;
+    if ($age >=18) {
+      echo "vous etes majeur";
+    }
+    else {
+      echo "vous etes mineur";
+    }
+     ?>
+  </body>
+</html>
